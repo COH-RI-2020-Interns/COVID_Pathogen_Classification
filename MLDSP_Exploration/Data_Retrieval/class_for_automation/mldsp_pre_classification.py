3d1a018fb1a065a510dbefc4d887ecb3bb65234d
EshaAnanth <22eshaananth@gmail.com>
2020-06-15 16:43:24 -0700

    updated branch

--- MLDSP_Exploration/Data_Retrieval/class_for_automation/mldsp_pre_classification.py
+++ MLDSP_Exploration/Data_Retrieval/class_for_automation/mldsp_pre_classification.py
@@ -10,21 +10,16 @@ class mlsdp_pre_classfication:
          self.file_path = file_path
          self.file_path2 = file_path2
 
-
-
-     # Sorting folders and selecting file
-     file_list = sorted(listdir(file_path))
-     file_list2 = sorted(listdir(file_path2))
-
-
      # Extracting string from file
-     def make_sequence1(file_path, number1):
+     def make_sequence1(number1):
+         file_list = sorted(listdir(file_path))
          ribo_example = list(SeqIO.parse(f"{file_path}/{file_list[number1]}", "fasta"))
          count = len(ribo_example[0].seq)
          seq = "".join([char for char in ribo_example[0].seq])
          return "This is the first sequence: /n" + seq
 
-     def make_sequence2(file_path2, number2):
+     def make_sequence2(number2):
+         file_list2 = sorted(listdir(file_path2))
          ribo_example = list(SeqIO.parse(f"{file_path2}/{file_list2[number2]}", "fasta"))
          count = len(ribo_example[0].seq)
          seq = "".join([char for char in ribo_example[0].seq])
@@ -32,5 +27,16 @@ class mlsdp_pre_classfication:
 
 
 
-first_test = mlsdp_pre_classfication("Test1", "Polyomaviridae", "0", "Test1", "Riboviria", "0")
-first_test.show_full_name()
+
+
+# Getting 1st user input
+test = input("Please input the 1st test you would like data from")
+folder = input("Please input the 1st folder you would like data from")
+folder
+# Getting 2nd user input
+test2 = input("Please input the 2nd test you would like data from")
+folder2 = input("Please input the 2nd folder you would like data from")
+
+# Getting file paths for both files
+file_path = getcwd() + f"/data/{test}/{folder}"
+file_path2 = getcwd() + f"/data/{test2}/{folder2}"
