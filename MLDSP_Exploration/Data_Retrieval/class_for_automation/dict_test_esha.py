738668deff39389a89c0e38b78ccac6742dc2263
EshaAnanth <22eshaananth@gmail.com>
2020-06-16 09:55:43 -0700

    updating dictionary file

--- MLDSP_Exploration/Data_Retrieval/class_for_automation/dict_test_esha.py
+++ MLDSP_Exploration/Data_Retrieval/class_for_automation/dict_test_esha.py
@@ -5,35 +5,36 @@ from Bio import SeqIO
 from os import getcwd, listdir, system
 from itertools import combinations
 
-fake_files = [f"file_{i+1}.fasta" for i in range(100)]
+#fake_files = [f"file_{i+1}.fasta" for i in range(100)]
 
-file_path = getcwd() + "/data/Fake_Data/Fake_Virus_1"
+file_path = getcwd() + "/data/Test1/Riboviria"
 
 #for file_name in fake_files:
     #system(f"touch {file_path}/{file_name}")
 
-fake_folder_path = getcwd() + "/data"
+folder_path = getcwd() + "/data"
 
-fake_folders = sorted(listdir(fake_folder_path))[1:3]
+folders = sorted(listdir(folder_path))[2:9]
+folders
 
-fake_folder_dict = {}
+folder_dict = {}
 
-for folder in fake_folders:
-    fake_subfolder_dict = {}
-    for sub_folder in listdir(f"{fake_folder_path}/{folder}"):
-        fake_subfolder_dict[sub_folder] = listdir(f"{fake_folder_path}/{folder}/{sub_folder}")
-    fake_folder_dict[folder] = fake_subfolder_dict
+for folder in folders:
+    subfolder_dict = {}
+    for sub_folder in listdir(f"{folder_path}/{folder}"):
+        subfolder_dict[sub_folder] = listdir(f"{folder_path}/{folder}/{sub_folder}")
+    folder_dict[folder] = subfolder_dict
 
-fake_folder_dict.keys()
+folder_dict.keys()
 
 output_path = getcwd() + "/data/JSON_Files"
 
-folder_json = json.dumps(fake_folder_dict)
+folder_json = json.dumps(folder_dict)
 
-system(f"touch {output_path}/fasta_files.json")
+#system(f"touch {output_path}/fasta_files.json")
 
 with open(f"{output_path}/fasta_files.json", "w") as my_file:
-    json.dump(fake_folder_dict, my_file)
+    json.dump(folder_dict, my_file)
 
 
 coords = [("ribo", 4,5), ("sarbecovirus", 2,8), ("ribo", 1,9), ("sarbecovirus", 20,8), ("ribo", 7,19)]
