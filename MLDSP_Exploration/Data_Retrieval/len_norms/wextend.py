c19e990a874c6a31710f7d2278e71c6147182fa0
anoushkabhat13 <anoushkabhat2016@gmail.com>
2020-06-17 16:29:42 -0700

    wextend pywt

--- MLDSP_Exploration/Data_Retrieval/len_norms/wextend.py
+++ MLDSP_Exploration/Data_Retrieval/len_norms/wextend.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pywt
 
 
-
-DNA_Seq = [ 1 ,-1 ,-1, 1 ]
+DNA_Seq = [  1 ,-1 ,-1, 1 ]
 len(DNA_Seq)
 #Goal = antisymmetric padding equal to a certain length
 antisymmetric_DNA = []
@@ -13,9 +13,9 @@ def antisymmetric(DNA_sequence, desired_length):
     num_to_add = (int)((desired_length - len(DNA_sequence))/2)
     middle = (int)(len(DNA_sequence)/2 - 1)
     for i in DNA_Seq[middle::-1]:
-        beg_num.append(i)
+        beg_num.append(i*-1)
     for j in DNA_Seq[len(DNA_Seq):middle:-1]:
-        end_num.append(j)
+        end_num.append(j*-1)
     for i in range(0,num_to_add):
         antisymmetric_DNA.append(beg_num[i])
     for i in DNA_sequence:
@@ -23,12 +23,18 @@ def antisymmetric(DNA_sequence, desired_length):
     for i in range(0,num_to_add):
         antisymmetric_DNA.append(end_num[i])
     return antisymmetric_DNA
-
-
-
 padded = antisymmetric(DNA_Seq, 8)
 padded
--1 1 1 -1 -1 1 -1
+
+
+print(pywt.Modes.modes)
+dna_seq = np.array(DNA_Seq)
+print(pywt.pad(dna_seq, 2, "antisymmetric"))
+
+
+
+
+
 
 DNA_Seq = [1,2,3,4]
 DNA_Seq[3:0]
