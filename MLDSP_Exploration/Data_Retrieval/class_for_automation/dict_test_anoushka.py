7ca52a9b15fc3ef8d96277dc3d45d32b4291c811
anoushkabhat13 <anoushkabhat2016@gmail.com>
2020-06-17 10:23:36 -0700

    mldsp exploration

--- MLDSP_Exploration/Data_Retrieval/class_for_automation/dict_test_anoushka.py
+++ MLDSP_Exploration/Data_Retrieval/class_for_automation/dict_test_anoushka.py
@@ -23,6 +23,7 @@ for folder in folders:
 
 
 folder_dict.keys()
+folder_dict.values()
 
 # Adding data to a JSON file
 output_path = getcwd() + "/data/JSON_Files"
@@ -38,7 +39,7 @@ my_dict.values()
 
 # Getting all possible combinations of 2 for the fasta files
 file_tuple_list = []
-dict_2 = {}
+
 for folder in folders:
     for sub_folder in listdir(f"{folder_path}/{folder}"):
         for file in listdir(f"{folder_path}/{folder}/{sub_folder}"):
@@ -46,12 +47,20 @@ for folder in folders:
 file_tuple_list
 
 file_combos = list(permutations(file_tuple_list, 2))
+my_dict["Test1"]
+len(my_dict)
+file_tuple_list_practice = {}
+
+for i in my_dict:
+   file_tuple_list_practice  = {}
+   file_tuple_list_practice = my_dict[i]
+   print(file_tuple_list_practice)
 
 
-for i in file_combos:
-    if (i[0][1] == i[1][1]):
-        file_combos.remove(i)
+file_tuple_list
 
 
+file_combos[6000000]
 
-#taking a while to go through everything
+
+ #taking a while to go through everything
