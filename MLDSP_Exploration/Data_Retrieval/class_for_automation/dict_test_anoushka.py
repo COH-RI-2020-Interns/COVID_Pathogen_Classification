dc53d5b3c848bbad27188e449ba21135976dc3d2
anoushkabhat13 <anoushkabhat2016@gmail.com>
2020-06-17 18:41:17 -0700

    updating automation

--- MLDSP_Exploration/Data_Retrieval/class_for_automation/dict_test_anoushka.py
+++ MLDSP_Exploration/Data_Retrieval/class_for_automation/dict_test_anoushka.py
@@ -9,7 +9,7 @@ from itertools import permutations
 folder_path = getcwd() + "/data"
 
 folders = sorted(listdir(folder_path))[1:8]
-folders
+
 
 folder_dict = {}
 
@@ -20,9 +20,6 @@ for folder in folders:
         subfolder_dict[sub_folder] = listdir(f"{folder_path}/{folder}/{sub_folder}")
     folder_dict[folder] = subfolder_dict
 
-list_1  = [1]
-list_2 = [3]
-list_1 + list_2
 
 
 folder_dict.keys()
@@ -38,30 +35,21 @@ with open(f"{output_path}/fasta_files.json", "w") as my_file:
 f = open(f"{output_path}/{listdir(output_path)[0]}", )
 
 my_dict = json.load(f)
-my_dict.values()
-my_dict["Test1"].keys()
+
+
 # Getting all possible combinations of 2 for the fasta files
 file_tuple_list = []
 
 for folder in folders:
     for sub_folder in listdir(f"{folder_path}/{folder}"):
-        print(sub_folder)
         for file in listdir(f"{folder_path}/{folder}/{sub_folder}"):
             file_tuple_list.append((folder,sub_folder,file))
-file_tuple_list
-
-
 
 
 file_combos = list(permutations(file_tuple_list, 2))
-file_combos
-
-
 
 new_dict = {}
 #each of the keys shows tuples, first element is the virus, second is file
-
-
 
 for i in my_dict.keys():
     file_list = []
@@ -70,7 +58,7 @@ for i in my_dict.keys():
             file_list.append((j,file))
         new_dict[i] = file_list
 
-new_dict
+
 
 new_dict_2 = {}
 for key in new_dict.keys():
@@ -78,10 +66,26 @@ for key in new_dict.keys():
     new_dict_2[key] =  seq_perm
 
 
+new_dict_2
+
 new_dict_3 = {}
 for key in new_dict_2.keys():
+    file_list_2 = []
     for i,j in new_dict_2[key]:
-        print(i)
+        if(i[0] != j[0]):
+            file_list_2.append((i,j))
+    new_dict_3[key] = file_list_2
+
+new_dict_3
+
+
+with open(f"{output_path}/final_permutations.json", "w") as my_file:
+    json.dump(new_dict_3, my_file)
+
+f = open(f"{output_path}/{listdir(output_path)[1]}", )
+
+perm_dict = json.load(f)
+
 
 len(my_dict)
 file_tuple_list_practice = {}
@@ -91,7 +95,6 @@ for i in my_dict:
    file_tuple_list_practice = my_dict[i]
    print(file_tuple_list_practice)
 
-file_perms
 
 file_tuple_list
 
