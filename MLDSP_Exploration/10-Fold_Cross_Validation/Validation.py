8a1bec4c8d262d89e16193cf91043af80d539c5a
EshaAnanth <22eshaananth@gmail.com>
2020-06-19 15:38:01 -0700

    following along with scikit learn

--- MLDSP_Exploration/10-Fold_Cross_Validation/Validation.py
+++ MLDSP_Exploration/10-Fold_Cross_Validation/Validation.py
@@ -1,7 +1,30 @@
-from sklearn import cross_validation
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
+from sklearn import metrics
+
+# Train Test split
+X, y = datasets.load_iris(return_X_y=True)
+X.shape, y.shape
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
+
+X_train.shape, y_train.shape
+X_test.shape, y_test.shape
+
+clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
+clf.score(X_test, y_test)
+
+# Cross validation
+clf = svm.SVC(kernel='linear', C=1)
+scores = cross_val_score(clf, X, y, cv=5)
+scores
+
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 
 
-
-
-data = cross_validation.KFold(len(train_set), n_folds=10, indices=False) 
+scores = cross_val_score(clf, X, y, cv=5, scoring='f1_macro')
+scores
