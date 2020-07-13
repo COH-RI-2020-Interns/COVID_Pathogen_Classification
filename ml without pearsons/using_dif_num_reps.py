7a462bc379b786d20d0ccd835db816cec48d1652
EshaAnanth <22eshaananth@gmail.com>
2020-07-13 14:35:33 -0700

    num reps for test 2

--- ml without pearsons/using_dif_num_reps.py
+++ ml without pearsons/using_dif_num_reps.py
@@ -30,12 +30,9 @@ for folder in folders:
         subfolder_dict[sub_folder] = listdir(f"{folder_path}/{folder}/{sub_folder}")
     folder_dict[folder] = subfolder_dict
 
-folder_dict.keys()
-
 
 #Adding data to a JSON file
 output_path = getcwd() + "/data/JSON_Files"
-
 
 with open(f"{output_path}/fasta_files.json", "w") as my_file:
     json.dump(folder_dict, my_file)
@@ -60,12 +57,12 @@ def entropy(sequence):
 rep_dict = {#"Int1":{"T":0,"t":0,"C":1,"c":1, "A":2,"a":2 ,"G":3, "g":3}}
 #"Int2": {"T":1,"t":1,"C":2,"c":2, "A":3,"a":3 ,"G":4, "g":4}}
 #"Real": {"T":-1.5,"t":-1.5,"C":0.5,"c":0.5, "A":1.5,"a":1.5 ,"G":-1.5, "g":-1.5}}
-"Atomic": {"T":6,"t":6,"C":58,"c":58, "A":70,"a":70 ,"G":78, "g":78}}
-#"EIIP": {"T":0.1335,"t":0.1335,"C":0.1340,"c":0.1340, "A":0.1260,"a":0.1260 ,"G":0.0806, "g":0.0806},
-#"PP": {"T":1,"t":1,"C":1,"c":1, "A":-1,"a":-1 ,"G":-1, "g":-1},
-#"Paired Numeric": {"T":1,"t":1,"C":-1,"c":-1, "A":1,"a":1 ,"G":-1, "g":-1},
-#"Just A": {"T":0,"t":0,"C":0,"c":0, "A":1,"a":1 ,"G":0, "g":0},
-#"Just C": {"T":0,"t":0,"C":1,"c":1, "A":0,"a":0 ,"G":0, "g":0},
+#"Atomic": {"T":6,"t":6,"C":58,"c":58, "A":70,"a":70 ,"G":78, "g":78}}
+#"EIIP": {"T":0.1335,"t":0.1335,"C":0.1340,"c":0.1340, "A":0.1260,"a":0.1260 ,"G":0.0806, "g":0.0806}}
+"PP": {"T":1,"t":1,"C":1,"c":1, "A":-1,"a":-1 ,"G":-1, "g":-1}}
+#"Paired Numeric": {"T":1,"t":1,"C":-1,"c":-1, "A":1,"a":1 ,"G":-1, "g":-1}}
+#"Just A": {"T":0,"t":0,"C":0,"c":0, "A":1,"a":1 ,"G":0, "g":0}}
+#"Just C": {"T":0,"t":0,"C":1,"c":1, "A":0,"a":0 ,"G":0, "g":0}}
 #"Just G": {"T":0,"t":0,"C":0,"c":0, "A":0,"a":0 ,"G":1, "g":1}}
 #"Just T": {"T":1,"t":1,"C":0,"c":0, "A":0,"a":0 ,"G":0, "g":0}}
 
@@ -95,35 +92,19 @@ file_path_1 = getcwd()
 entropy_dict = {}
 # for test in my_dict.keys():
 entropy_values = []
-for family in my_dict["Test3b"].keys():
-    for file in my_dict["Test3b"][family]:
-        start_seq = list(SeqIO.parse((f"{file_path_1}/data/Test3b/{family}/{file}"), "fasta"))
+for family in my_dict["Test2"].keys():
+    for file in my_dict["Test2"][family]:
+        start_seq = list(SeqIO.parse((f"{file_path_1}/data/Test2/{family}/{file}"), "fasta"))
         count = len(start_seq[0].seq)
         final_seq = "".join([char for char in start_seq[0].seq])
         entropy_values.append((family, magtropy(final_seq)[0]))
 
 
-entropy_dict["Test3b"] = entropy_values
+entropy_dict["Test2"] = entropy_values
 
-#test1 = pd.DataFrame.from_dict(entropy_dict["Test1"])
-#test2 = pd.DataFrame.from_dict(entropy_dict["Test2"])
-#test3a = pd.DataFrame.from_dict(entropy_dict["Test3a"])
-test3b = pd.DataFrame.from_dict(entropy_dict["Test3b"])
-#test4 = pd.DataFrame.from_dict(entropy_dict["Test4"])
-#test5  = pd.DataFrame.from_dict(entropy_dict["Test5"])
-#test6 = pd.DataFrame.from_dict(entropy_dict["Test6"])
-# test8 = pd.DataFrame.from_dict(entropy_dict["Test8"])
+test2 = pd.DataFrame.from_dict(entropy_dict["Test2"])
 
-#test1.columns = ["Family", "int1", "int2", "EIIP", "JustA", "JustG"]
-#test2.columns = ["Family", "int1", "int2", "EIIP", "JustA", "JustG"]
-#test3a.columns = ["Family", "int1", "Real", "EIIP", "JustA", "JustG"]
-test3b.columns = ["Family", "int1"]
-#test3b.columns = ["Family", "int1", "int2", "EIIP", "JustA", "JustG"] #, "int2", "Real", "Atomic", "EIIP", "PP", "Paired Numeric", "JustA", "JustC", "JustG", "JustT"]
-
-#test4.columns = ["Family", "int1", "int2", "EIIP", "JustA", "JustG"]
-#test5.columns = ["Family", "int1", "int2", "EIIP", "JustA", "JustG"]
-#test6.columns = ["Family", "Magtropy"]
-#test8.columns = ["Family", "Magtropy"]
+test2.columns = ["Family", "int1"]
 
 # Hypertuning
 model_dict = {'log': LogisticRegression(),
@@ -134,9 +115,9 @@ model_dict = {'log': LogisticRegression(),
              'decision_tree': DecisionTreeClassifier()
                 }
 
-X = test3b.drop(columns = ["Family"])
+X = test2.drop(columns = ["Family"])
 
-y = pd.DataFrame(test3b["Family"])
+y = pd.DataFrame(test2["Family"])
 
 data_path = getcwd() + "/data/JSON_Files"
 #opening the json file that contains all the different parameters of each classification model
@@ -193,9 +174,6 @@ entropy_dict["Test8"] = entropy_values
 
 df2 = pd.DataFrame.from_dict(entropy_dict["Test8"])
 df2.columns = ["Family", "int1"] #, "int2", "EIIP", "JustA", "JustG"]
-#df2.columns = ["Family", "int1", "int2", "Real", "Atomic", "EIIP", "PP", "Paired Numeric", "JustA", "JustC", "JustG", "JustT"]
 
 df2 = df2.drop(columns = ["Family"])
 my_model.predict(df2)
-
-# Doing entropy divided by magnitude_average removed Caudovirales from the classification of test3a
