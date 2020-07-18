1f53f303bdf94d6c5a57afb92b33a0d523ec7843
EshaAnanth <22eshaananth@gmail.com>
2020-07-18 12:25:27 -0700

    commits

--- NCBI_classification/classification_magtropy.py
+++ NCBI_classification/classification_magtropy.py
@@ -18,8 +18,8 @@ from sklearn.tree import DecisionTreeClassifier
 #Going to Test folders
 folder_path = getcwd() + "/data2"
 
-folders = sorted(listdir(folder_path))[0:12]
-folders
+folders = sorted(listdir(folder_path))[0:13]
+#folders
 
 folder_dict = {}
 
@@ -46,7 +46,7 @@ for test in my_dict:
 
 #Dictionary of numerical representations
 rep_dict = {#"Int1":{"T":0,"t":0,"C":1,"c":1, "A":2,"a":2 ,"G":3, "g":3},
-"Int2": {"T":1,"t":1,"C":2,"c":2, "A":3,"a":3 ,"G":4, "g":4},
+#"Int2": {"T":1,"t":1,"C":2,"c":2, "A":3,"a":3 ,"G":4, "g":4},
 "Real": {"T":-1.5,"t":-1.5,"C":0.5,"c":0.5, "A":1.5,"a":1.5 ,"G":-1.5, "g":-1.5},
 "EIIP": {"T":0.1335,"t":0.1335,"C":0.1340,"c":0.1340, "A":0.1260,"a":0.1260 ,"G":0.0806, "g":0.0806},
 "PP": {"T":1,"t":1,"C":1,"c":1, "A":-1,"a":-1 ,"G":-1, "g":-1},
@@ -103,7 +103,7 @@ def magtropy_dict(sublevel):
     return sublevel
 
 #Preparing training data for supervised machine learning
-sublevel_df = magtropy_dict("8_Subfamily")
+sublevel_df = magtropy_dict("8_Subfamily_Partial")
 X = sublevel_df.drop(columns = ["Sublevel Name"])    #these are the training features
 y = pd.DataFrame(sublevel_df["Sublevel Name"])       #this are the target labels
 
@@ -164,7 +164,7 @@ my_model = ML_Pipeline(X, y, "svm", 10, 0.2)
 
 #Testing data of COVID-19 Files
 covid_df = magtropy_dict("0_COVID")
-X = covid_df.drop(columns = ["Sublevel Name"])    #these are the testing features
+X_test = covid_df.drop(columns = ["Sublevel Name"])    #these are the testing features
 
-my_model.predict(X)
-my_model.predict_proba(X)
+my_model.predict(X_test)
+#my_model.predict_proba(X1)
