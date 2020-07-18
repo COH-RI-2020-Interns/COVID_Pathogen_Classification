a0b240512c4461fdd4cdf034585abcdb94e63016
EshaAnanth <22eshaananth@gmail.com>
2020-07-17 18:01:09 -0700

    updates

--- NCBI_classification/classification_magtropy.py
+++ NCBI_classification/classification_magtropy.py
@@ -18,7 +18,7 @@ from sklearn.tree import DecisionTreeClassifier
 #Going to Test folders
 folder_path = getcwd() + "/data2"
 
-folders = sorted(listdir(folder_path))[0:9]
+folders = sorted(listdir(folder_path))[0:12]
 folders
 
 folder_dict = {}
@@ -45,7 +45,7 @@ for test in my_dict:
 
 
 #Dictionary of numerical representations
-rep_dict = {"Int1":{"T":0,"t":0,"C":1,"c":1, "A":2,"a":2 ,"G":3, "g":3},
+rep_dict = {#"Int1":{"T":0,"t":0,"C":1,"c":1, "A":2,"a":2 ,"G":3, "g":3},
 "Int2": {"T":1,"t":1,"C":2,"c":2, "A":3,"a":3 ,"G":4, "g":4},
 "Real": {"T":-1.5,"t":-1.5,"C":0.5,"c":0.5, "A":1.5,"a":1.5 ,"G":-1.5, "g":-1.5},
 "EIIP": {"T":0.1335,"t":0.1335,"C":0.1340,"c":0.1340, "A":0.1260,"a":0.1260 ,"G":0.0806, "g":0.0806},
@@ -54,8 +54,7 @@ rep_dict = {"Int1":{"T":0,"t":0,"C":1,"c":1, "A":2,"a":2 ,"G":3, "g":3},
 "Just A": {"T":0,"t":0,"C":0,"c":0, "A":1,"a":1 ,"G":0, "g":0},
 "Just C": {"T":0,"t":0,"C":1,"c":1, "A":0,"a":0 ,"G":0, "g":0},
 "Just G": {"T":0,"t":0,"C":0,"c":0, "A":0,"a":0 ,"G":1, "g":1},
-"Just T": {"T":1,"t":1,"C":0,"c":0, "A":0,"a":0 ,"G":0, "g":0}
-rep_dict
+"Just T": {"T":1,"t":1,"C":0,"c":0, "A":0,"a":0 ,"G":0, "g":0}}
 
 
 # Finding the Average Magnitude of the Sequence
@@ -105,7 +104,7 @@ def magtropy_dict(sublevel):
 
 #Preparing training data for supervised machine learning
 sublevel_df = magtropy_dict("8_Subfamily")
-X = sublevel_df.drop(columns = ["Sublevel Name"])    #these are the features
+X = sublevel_df.drop(columns = ["Sublevel Name"])    #these are the training features
 y = pd.DataFrame(sublevel_df["Sublevel Name"])       #this are the target labels
 
 
